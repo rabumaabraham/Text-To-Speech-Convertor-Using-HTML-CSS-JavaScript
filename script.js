0e2ea9e6ce33648e78ce31b0e7dd16f59c94cfb2
rabuma abraham <iamrabuma@gmail.com>
2024-10-21 19:23:54 +0200

    bug fixed from voice selection dropdown

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-// letting the button speak
-
+// Letting the button speak
 let speech = new SpeechSynthesisUtterance();
 let text = document.querySelector("textarea");
-let mybtn = document.querySelector("button")
+let mybtn = document.querySelector("button");
 
-function listen(){
+function listen() {
     speech.text = text.value;
     window.speechSynthesis.speak(speech);
 }
 
 mybtn.addEventListener("click", listen);
 
-
-// selecting voices
-
+// Selecting voices
 let voices = [];
 let voiceSelect = document.querySelector("select");
 
-function myFunction(){
-    window.speechSynthesis.onvoiceschanged = function(){
+function myFunction() {
+    window.speechSynthesis.onvoiceschanged = function() {
         voices = window.speechSynthesis.getVoices();
         speech.voice = voices[0];
 
         voiceSelect.innerHTML = '';
-
-        voices.forEach(function(voice, i){
+        voices.forEach(function(voice, i) {
             voiceSelect.options[i] = new Option(voice.name, i);
-        })
-        voiceSelect.addEventListener("change", function(){
+        });
+
+        voiceSelect.addEventListener("change", function() {
             speech.voice = voices[voiceSelect.value];
         });
+    };
+
+    // Load voices immediately if available
+    voices = window.speechSynthesis.getVoices();
+    if (voices.length) {
+        voices.forEach(function(voice, i) {
+            voiceSelect.options[i] = new Option(voice.name, i);
+        });
+        speech.voice = voices[0]; // Set default voice
     }
 }
 
